295bd0be9af370cea8c9a870bd031774e210a12e
Min Min Latt <minminlaxz@gmail.com>
2024-07-14 17:24:08 +0630

    Update docker-images.sh

--- docker-images.sh
+++ docker-images.sh
@@ -1,3 +1,5 @@
+# https://deepstack.readthedocs.io/en/latest/nvidia-jetson/
 docker pull deepquestai/deepstack:jetpack-2022.01.1
+
 docker pull eclipse-mosquitto:latest
 docker pull ghcr.io/blakeblackshear/frigate:stable-tensorrt-jp4
